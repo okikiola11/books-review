109d421400ebffd229fe299713ffa3f13937aaea
Okikiola Apelehin <okikiola.apelehin@gmail.com>
2024-02-15 16:10:08 +0100

    Chore: Implement the popular method to the Book model Implement the HighestRated to the Book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,8 +10,32 @@ class Book extends Model
 {
     use HasFactory;
 
-    public function reviews() 
+    protected $fillable = [ 'title' ];
+
+    public function reviews()
     {
         return $this->hasMany(Review::class);
     }
+
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    public function scopePopular(Builder $query): Builder
+    {
+        // Sort the books with the ones with the most reviews
+        return $query->withCount('reviews')
+                    ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query): Builder
+    {
+        return $query->withAvg('reviews', 'rating')
+                    ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    // Using s Real Life Scenario
+    // Using Movies bcos their popularity fades away
+    public 
 }
